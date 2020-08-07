eae02adab47eebfacdca2fa549caedb2f49ef7f5
Comyune Technologies <comyune@gmail.com>
2020-08-07 21:06:16 +0100

    Update test scripts to show orphaned nodes

--- scripts/test-unit.sh
+++ scripts/test-unit.sh
@@ -2,7 +2,7 @@
 pushd godot-project
 
 GODOT=${GODOT_PATH:-'/Applications/Godot.app/Contents/MacOS/Godot'}
-DEFAULT_ARGUMENTS="-s addons/gut/gut_cmdln.gd -ghide_orphans --path $PWD -gexit $@"
+DEFAULT_ARGUMENTS="-s addons/gut/gut_cmdln.gd --path $PWD -gexit $@"
 
 echo "#=== Running Godot unit tests..."
 $GODOT $DEFAULT_ARGUMENTS -gdir=res://test/unit || exit 1
