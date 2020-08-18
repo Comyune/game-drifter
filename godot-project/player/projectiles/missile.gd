706f0d9ba66150ddbd7b02f52fb22d79610ca239
Comyune Technologies <comyune@gmail.com>
2020-08-18 15:13:59 +0100

    Missile shooting with decent impulse and forces

--- godot-project/player/projectiles/missile.gd
+++ godot-project/player/projectiles/missile.gd
@@ -1,6 +1,16 @@
 extends RigidBody2D
 
-export (float) var force = 400
+export (float) var initial_impulse  = 200.0
+export (float) var acceleration     = 10.0
+export (float) var maximum_velocity = 300.0
+var force = Vector2(0, -acceleration)
 
-func _physics_process(delta):
-	pass
+func _enter_tree():
+	apply_impulse(
+		Vector2.ZERO,
+		Vector2(0, -initial_impulse).rotated(rotation)
+	)
+
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	if state.get_linear_velocity().length() > maximum_velocity: return
+	add_central_force(force.rotated(rotation))
