67077042201583cc92a5904d780e702c10e60af8
Comyune Technologies <comyune@gmail.com>
2020-08-18 15:25:03 +0100

    Missiles colliding and dying

--- godot-project/player/projectiles/missile.gd
+++ godot-project/player/projectiles/missile.gd
@@ -5,7 +5,9 @@ export (float) var acceleration     = 10.0
 export (float) var maximum_velocity = 300.0
 var force = Vector2(0, -acceleration)
 
-func _enter_tree():
+func _ready():
+	var _connection = connect("body_entered", self, "_on_body_entered")
+
 	apply_impulse(
 		Vector2.ZERO,
 		Vector2(0, -initial_impulse).rotated(rotation)
@@ -14,3 +16,6 @@ func _enter_tree():
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	if state.get_linear_velocity().length() > maximum_velocity: return
 	add_central_force(force.rotated(rotation))
+
+func _on_body_entered(_body): die()
+func die(): queue_free()
